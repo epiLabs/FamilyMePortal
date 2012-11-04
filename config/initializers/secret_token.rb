24d7ea2088a428d38114b9a78575f023aeb32bbb
Dimitri Jorge <jorge.dimitri@gmail.com>
2012-11-04 21:32:31 +0000

    Create the rails app: First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilyMe::Application.config.secret_token = '4c8fe1197b32924d04b26f6f4a5b46f1ac868d904cfbd395960e8b7ec43681d0776ea72d412314299fe9e0bb546dbddd888bc4d2e48b31d7684393f74b8c5612'
