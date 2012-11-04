aed260bfc5bf69442365ee1e16bda1560127994e
Dimitri Jorge <jorge.dimitri@gmail.com>
2012-11-04 22:36:41 +0000

    Implement devise for users

--- db/migrate/20121104222707_create_users.rb
+++ db/migrate/20121104222707_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name, uniqueness: true, presence: true
-      t.string :email, uniqueness: true, presence: true
+
       t.timestamps
     end
   end
